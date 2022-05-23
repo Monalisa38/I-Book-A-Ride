b2ed7be020a814bbe19ffaef9f22175ea03e4e57
Monalisa38 <84154952+Monalisa38@users.noreply.github.com>
2022-05-23 16:15:12 +1200

    Create DBManager.java

---
+++ DBManager.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg603_assignment2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Monalisa
+ */
+public class DBManager {
+    private static final String USER_NAME = "pdc2"; //your DB username
+    private static final String PASSWORD = "pdc2"; //your DB password
+    private static final String URL = "jdbc:derby:603_Travel;create=true";  //"jdbc:derby://localhost:1527/BookStore";  //url of the DB host
+
+    Connection conn;
+
+    public DBManager() {
+        establishConnection();
+    }
+
+    public static void main(String[] args) {
+        DBManager dbManager = new DBManager();
+        System.out.println(dbManager.getConnection());
+    }
+
+    public Connection getConnection() {
+        return this.conn;
+    }
+
+    //Establish connection
+    public void establishConnection() {
+        try {
+            conn = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+            System.out.println(URL+"connected....");
+        } catch (SQLException ex) {
+           System.err.println("SQL Exception: "+ex.getMessage());
+        }
+        
+        
+    }
+
+    public void closeConnections() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+    public ResultSet queryDB(String sql) {
+
+        Connection connection = this.conn;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sql);
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return resultSet;
+    }
+
+    public void updateDB(String sql) {
+
+        Connection connection = this.conn;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            statement = connection.createStatement();
+            statement.executeUpdate(sql);
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
