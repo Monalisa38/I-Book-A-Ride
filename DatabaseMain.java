4ebf35a0ec76e0fac987271e9593937ddf3a3325
Monalisa38 <84154952+Monalisa38@users.noreply.github.com>
2022-05-23 16:16:10 +1200

    Create DatabaseMain.java

---
+++ DatabaseMain.java
@@ -0,0 +1,20 @@
+package pkg603_assignment2;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Monalisa
+ */
+public class DatabaseMain {
+    
+    public static void main(String[] args) {
+    DBManager db = new DBManager();
+    
+
+       }
+    
+}
