1b36a280e387eb1e65643cda067f8edd9f6dd3a0
Monalisa38 <84154952+Monalisa38@users.noreply.github.com>
2022-05-25 20:58:10 +1200

    Update PersonalDetailsDB.java

--- PersonalDetailsDB.java
+++ PersonalDetailsDB.java
@@ -49,12 +49,14 @@ public void createPersonalDetailsTable() {
         String newTable = "PERSONALDETAILS";
 
 
-        String sqlCreateTable = "CREATE TABLE "+newTable+" (Name VARCHAR(10),"+"Email VARCHAR(50)," +"PhoneNumber INT,"+ "Age INT)";
-        String sqlAddRecord1 = "INSERT INTO PERSONALDETAILS VALUES('Monalisa','abc', 1234567890, 19)";
-
+        String sqlCreateTable = "CREATE TABLE "+newTable+" (Name VARCHAR(255),"+"Email VARCHAR(255)," +"PhoneNumber INT,"+ "Age INT)";
+        String sqlAddRecord1 = "INSERT INTO PERSONALDETAILS VALUES('Monalisa','abc@yahoo.com', 1234567890, 19)";
+        String sqlAddRecord2 = "INSERT INTO PERSONALDETAILS VALUES('Alisha','abcd@yahoo.com', 1234567890, 15)";
+        
         this.statement = conn.createStatement();
         this.statement.addBatch(sqlCreateTable);
         this.statement.addBatch(sqlAddRecord1);
+        this.statement.addBatch(sqlAddRecord2);
         this.statement.executeBatch();
         
         }
@@ -67,6 +69,58 @@ public void createPersonalDetailsTable() {
 
 
 }
+public void queryName() throws SQLException
+{
+    ResultSet rs = null;
+    try{
+        rs = this.statement.executeQuery("SELECT Name from PERSONALDETAILS");
+        while(rs.next())
+        {
+            String name= rs.getString("Name");
+            System.out.println("Name: " +(name));
+        }
+    }
+    catch(SQLException ex)
+    {
+        System.out.println("[queryName]:"+ex.getMessage());
+    }
+}
+
+public void queryEmail() throws SQLException
+{
+    ResultSet rs = null;
+    try{
+        rs = this.statement.executeQuery("SELECT Email from PERSONALDETAILS");
+        while(rs.next())
+        {
+            String email= rs.getString("Email");
+            System.out.println("Email: " +(email));
+        }
+    }
+    catch(SQLException ex)
+    {
+        System.out.println("[queryEmail]:"+ex.getMessage());
+    }
+}
+
+public void queryPhoneNumber() throws SQLException
+{
+    ResultSet rs = null;
+    try{
+        rs = this.statement.executeQuery("SELECT PhoneNumber from PERSONALDETAILS");
+        while(rs.next())
+        {
+            int phoneNumber = rs.getInt("PhoneNumber");
+            System.out.println("Phone Number: " + Integer.toString(phoneNumber));
+        }
+    }
+    catch(SQLException ex)
+    {
+        System.out.println("[queryPhoneNumber]:"+ex.getMessage());
+    }
+}
+    
+
 
 public void queryAge() throws SQLException
 {
@@ -76,7 +130,7 @@ public void queryAge() throws SQLException
         while(rs.next())
         {
             int age = rs.getInt("Age");
-            System.out.println("age: " + Integer.toString(age));
+            System.out.println("Age: " + Integer.toString(age));
         }
     }
     catch(SQLException ex)
