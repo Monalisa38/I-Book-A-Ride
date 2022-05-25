e8fdfc0d486264b46550358fab6b0aaf37c5f983
Monalisa38 <84154952+Monalisa38@users.noreply.github.com>
2022-05-25 20:56:54 +1200

    Update MembershipDetailsDB.java

--- MembershipDetailsDB.java
+++ MembershipDetailsDB.java
@@ -1,4 +1,4 @@
-/*
+    /*
      * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
      * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
      */
@@ -39,6 +39,68 @@
 
 
     }
+    
+        public void createMembershipTable() {
+
+            try{
+
+            Statement statement = conn.createStatement();
+            this.checkExistedTable("MembershipTable");
+            String newTable3 = "MEMBERSHIPTABLE";
+
+
+            String sqlCreateTable3 = "CREATE TABLE "+newTable3+" (Name VARCHAR(255)"+"YesorNo VARCHAR(4)";
+            String sqlAddRecord1 = "INSERT INTO PERSONALDETAILS VALUES('Monalisa', 'Yes')";
+
+            this.statement = conn.createStatement();
+            this.statement.addBatch(sqlCreateTable3);
+            this.statement.addBatch(sqlAddRecord1);
+            this.statement.executeBatch();
+            }
+           catch(SQLException ex)
+            {
+            System.err.println("SQLException: "+ ex.getMessage());
+
+            }   
+
+
+
+    }
+        
+        public void queryName() throws SQLException
+{
+    ResultSet rs = null;
+    try{
+        rs = this.statement.executeQuery("SELECT Name from MEMBERSHIPDETAILS");
+        while(rs.next())
+        {
+            String name= rs.getString("Name");
+            System.out.println("Name: " +(name));
+        }
+    }
+    catch(SQLException ex)
+    {
+        System.out.println("[queryName]:"+ex.getMessage());
+    }
+}
+        
+        
+   public void queryMembership() throws SQLException
+{
+    ResultSet rs = null;
+    try{
+        rs = this.statement.executeQuery("SELECT YesorNo from MEMBERSHIPDETAILS");
+        while(rs.next())
+        {
+            String yon= rs.getString("YesorNo");
+            System.out.println("YesorNo: " +(yon));
+        }
+    }
+    catch(SQLException ex)
+    {
+        System.out.println("[queryYesorNo]:"+ex.getMessage());
+    }
+}
 
     public void checkExistedTable(String name)
     {
@@ -69,30 +131,7 @@
 
 
 
-    public void createMembershipTable() {
 
-            try{
-
-            Statement statement = conn.createStatement();
-            this.checkExistedTable("MembershipTable");
-            String newTable3 = "MEMBERSHIPTABLE";
-
-
-            String sqlCreateTable3 = "CREATE TABLE "+newTable3+" (YesorNo VARCHAR(4),ID INT)";
-
-            this.statement = conn.createStatement();
-            this.statement.addBatch(sqlCreateTable3);
-            this.statement.executeBatch();
-            }
-           catch(SQLException ex)
-            {
-            System.err.println("SQLException: "+ ex.getMessage());
-
-            }   
-
-
-
-    }
     public void closeConnection()
     {
         this.dbManager.closeConnections();
