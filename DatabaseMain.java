9c12356cf41ec0fdf1987e2c19fadd4300b81b54
Monalisa38 <84154952+Monalisa38@users.noreply.github.com>
2022-05-30 15:26:14 +1200

    Update DatabaseMain.java

--- DatabaseMain.java
+++ DatabaseMain.java
@@ -1,53 +1,58 @@
-    package pkg603_assignment2;
+package pkg603_assignment2;
 
-    import java.sql.SQLException;
+import java.sql.SQLException;
 
-    /*
+/*
      * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
      * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
-     */
+ */
+/**
+ *
+ * @author Monalisa
+ */
+public class DatabaseMain {
 
-    /**
-     *
-     * @author Monalisa
-     */
-    public class DatabaseMain {
-
-        public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException {
         DBManager db = new DBManager();
 
-        PersonalDetailsDB dt= new PersonalDetailsDB();
+        PersonalDetailsDB dt = new PersonalDetailsDB();
         TravelDetailsDB dt1 = new TravelDetailsDB();
         PaymentDetailsDB dt2 = new PaymentDetailsDB();
         MembershipDetailsDB dt3 = new MembershipDetailsDB();
 
-           dt.connectTravelDB();
-           dt.createPersonalDetailsTable();
-           dt.queryName();
-           dt.queryEmail();
-           dt.queryPhoneNumber();
-           dt.queryAge();
-           dt.closeConnection();
+        dt.connectTravelDB();
+        dt.createPersonalDetailsTable();
+        dt.queryName();
+        dt.queryEmail();
+        dt.queryPhoneNumber();
+        dt.queryAge();
+        dt.closeConnection();
 
-           dt1.connectTravelDB();
-           dt1.createTravelDetailsTable();
-           dt1.queryName();
-           dt1.queryTravelType();
-           dt1.queryCity();
-           dt1.queryPeople();
-           dt1.queryDate();
-           dt1.closeConnection();
+        dt1.connectTravelDB();
+        dt1.createTravelDetailsTable();
+        dt1.queryName();
+        dt1.queryTravelType();
+        dt1.queryCity();
+        dt1.queryPeople();
+        dt1.queryDate();
+        dt1.closeConnection();
 
-           dt2.connectTravelDB();
-           dt2.createPaymentDetailsTable();
-           dt2.closeConnection();
+        dt2.connectTravelDB();
+        dt2.createPaymentDetailsTable();
+        dt2.queryName();
+        dt2.queryPaymentType();
+        dt2.queryCardHolderName();
+        dt2.queryCardNumber();
+        dt2.queryCVV();
+        dt2.queryCardExpiryDate();
+        dt2.closeConnection();
 
-           dt3.connectTravelDB();
-           dt3.createMembershipTable();
-           dt3.closeConnection();
-
-
-
-           }
+        dt3.connectTravelDB();
+        dt3.createMembershipDetailsTable();
+        dt3.queryName();
+        dt3.queryMembership();
+        dt3.closeConnection();
 
     }
+
+}
