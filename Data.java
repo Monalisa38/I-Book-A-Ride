009ca6af3564614df45d913020238f5765ced5f0
Monalisa38 <84154952+Monalisa38@users.noreply.github.com>
2022-06-02 12:00:18 +1200

    Create Data.java

---
+++ Data.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg603_assignment2;
+
+/**
+ *
+ * @author Monalisa
+ */
+public class Data {
+    boolean name= false; // Check if the user successfully added details into the database
+    boolean entered = false;
+    boolean quitFlag = false; // Check if the user quit the system.
+
+
+}
