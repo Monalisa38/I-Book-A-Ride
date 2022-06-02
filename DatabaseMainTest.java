014561e24dcbaea9bd8299ac5648418380524e80
Monalisa38 <84154952+Monalisa38@users.noreply.github.com>
2022-06-02 12:35:02 +1200

    Create DatabaseMainTest.java

---
+++ DatabaseMainTest.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package pkg603_assignment2;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Monalisa
+ */
+public class DatabaseMainTest {
+    
+    public DatabaseMainTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class DatabaseMain.
+     */
+    @Test
+    public void testMain() throws Exception {
+        System.out.println("main");
+        String[] args = null;
+        DatabaseMain.main(args);
+
+
+    }
+    
+}
